93649d9f469a577323d3f837a60981e2ecceac9e
Sebastian Bittmann <sehrbastian@Sebastians-MacBook-Pro.local>
2015-10-04 21:14:51 +0200

    browser is ready

--- src/main/java/org/bpmnbrowser/bpmn20xmltoimg/service/bpmn20xmltoimgservice.java
+++ src/main/java/org/bpmnbrowser/bpmn20xmltoimg/service/bpmn20xmltoimgservice.java
@@ -4,14 +4,16 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
+import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 
 import java.nio.file.StandardOpenOption;
+import java.util.Enumeration;
 import java.util.Iterator;
-
+import java.util.stream.Stream;
 
 import javax.servlet.ServletContext;
 import javax.ws.rs.Consumes;
@@ -31,7 +33,7 @@ import org.bpmnbrowser.bpmn20xmltoimg.BPMN20XMLParser;
 public class bpmn20xmltoimgservice {
 
 	static String RESOURCES_DIR = "/resources";
-	
+
 	@Context
 	private ServletContext context;
 
@@ -39,35 +41,89 @@ public class bpmn20xmltoimgservice {
 		//TODO: is it necessary to create the resources directory here, if not available?
 		return context.getRealPath(RESOURCES_DIR);
 	}
-	
-	
+
+
+	@GET
+	@Produces("application/json")
+	public Response getBPMN(){
+		String resourcePathString = getResourcePath();
+		java.nio.file.Path resourcePath = FileSystems.getDefault().getPath(resourcePathString);
+		StringBuilder result = new StringBuilder();
+		try {
+			String contextPath = context.getContextPath();
+
+			result.append("{");
+			String wd = walkDirectory(resourcePath,1,contextPath+"/bpmn/");
+			result.append(wd.substring(1));
+			result.append("}");
+			return Response.ok(result.toString()).build();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			return Response.status(404).build();
+		}finally{
+
+		}
+	}
+
+
+	private String walkDirectory(java.nio.file.Path resourcePath, int i,String prefix) throws IOException {
+		StringBuilder result = new StringBuilder();
+		Stream<java.nio.file.Path> folders = Files.walk(resourcePath, 1);
+		Iterator<java.nio.file.Path> iter = folders.iterator();
+		
+		while(iter.hasNext()){
+			java.nio.file.Path next = iter.next();
+
+			if(!Files.isSameFile(next, resourcePath) &&  Files.isDirectory(next)){
+				if(i>0){
+
+					String walkedDirectory = walkDirectory(next, i-1,prefix+next.getFileName().toString());	
+					result.append(walkedDirectory);
+
+				}else{
+					result.append(",");
+					result.append("\"");
+					result.append(resourcePath.getFileName().toString());
+					result.append("/");
+					result.append(next.getFileName().toString());
+					result.append("\"");
+					result.append(":");
+					result.append("\"");
+					result.append(prefix);
+					result.append("/");
+					result.append(next.getFileName().toString());
+					result.append("\"");
+				}
+
+			};
+		}
+		folders.close();
+		return result.toString();
+	}
+
+
 	@PUT
 	@Path("/{group}/{name}")
 	@Produces("application/xml")
 	@Consumes(MediaType.APPLICATION_XML)
 	public Response setBPMNDiagram(@PathParam(value = "group") String group, @PathParam(value = "name") String name,String requestbody){
-		
+
 		try{
 			BPMN20XMLParser parser = new BPMN20XMLParser();
 			InputStream png = parser.parseXML(requestbody);
-			
-			
+
 			//Create Folders
 			String resourcePath = getResourcePath();
 			//java.nio.file.Path path = FileSystems.getDefault().getPath(resourcePath);
 			java.nio.file.Path newFolders = Paths.get(resourcePath, group,name);
 			Files.createDirectories(newFolders);
-			
+
 			//Save xml!
 			String finalPath = context.getRealPath(RESOURCES_DIR+"/"+group+"/"+name+"/standard.bpmn20.xml");
 			java.nio.file.Path savePath = FileSystems.getDefault().getPath(finalPath);
-			
+
 			Files.deleteIfExists(savePath);
-			
 			Files.write(savePath, requestbody.getBytes(), StandardOpenOption.CREATE);
-			//Files.copy(requestbody, savePath, StandardCopyOption.REPLACE_EXISTING);
-			
-			
 			return Response.ok().build();
 		}catch(NoSuchFileException e){
 			return Response.status(404).build();
@@ -87,10 +143,10 @@ public class bpmn20xmltoimgservice {
 
 		PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:"+realPath+"/*.xml");
 
-		
-		
+
+
 		try {
-			
+
 			Iterator<java.nio.file.Path> iterator = Files.walk(subRoot).iterator();
 			while(iterator.hasNext()){
 				java.nio.file.Path next = iterator.next();
@@ -98,14 +154,14 @@ public class bpmn20xmltoimgservice {
 					input = next;
 				}
 			};
-			
+
 			if(input == null ){
 				return Response.status(404).build();
 			}
-			
-			
+
+
 			String np = input.toString().substring(0, input.toString().lastIndexOf("."));
-			
+
 			java.nio.file.Path output = FileSystems.getDefault().getPath(np+".png");
 
 			BPMN20XMLParser parser = new BPMN20XMLParser();
